139fb0c1c88bd0f2eeb8110596bddcaa76a78fc4
John Doe <johndoe@example.com>
2023-06-05 11:27:44 +0800

    1

--- src/config/admin.php
+++ src/config/admin.php
@@ -5,18 +5,18 @@ use App\Ebcms\Cloud\Http\Index;
 use PsrPHP\Framework\Framework;
 use PsrPHP\Router\Router;
 
-return Framework::execute(function (
-    Account $account,
-    Router $router
-): array {
-    $menus = [];
-    if ($account->checkAuth(Index::class)) {
-        $menus[] = [
-            'url' => $router->build('/ebcms/cloud/index'),
-            'title' => '系统升级',
-        ];
-    }
-    return [
-        'menus' => $menus,
-    ];
-});
+return [
+    'menus' => Framework::execute(function (
+        Account $account,
+        Router $router
+    ): array {
+        $menus = [];
+        if ($account->checkAuth(Index::class)) {
+            $menus[] = [
+                'url' => $router->build('/ebcms/cloud/index'),
+                'title' => '系统升级',
+            ];
+        }
+        return $menus;
+    }),
+];
